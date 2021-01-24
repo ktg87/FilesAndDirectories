f4a929a8f8b47e2b3c20118a87ff612b5bef71d5
Keith G <kguydo@gmail.com>
2021-01-24 08:01:06 -0500

    Initial Commit

---
+++ FilesAndDirectories/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FilesAndDirectories
+//
+//  Created by Keith Guydo on 1/24/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
