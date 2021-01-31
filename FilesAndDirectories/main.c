1b573f83692c043dcccdae00b2308136a971074d
Keith G <kguydo@gmail.com>
2021-01-31 08:48:29 -0500

    Added some system call features for creating a file.

--- FilesAndDirectories/main.c
+++ FilesAndDirectories/main.c
@@ -1,14 +1,23 @@
-//
-//  main.c
-//  FilesAndDirectories
-//
-//  Created by Keith Guydo on 1/24/21.
-//
-
 #include <stdio.h>
+#include <fcntl.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int file_descriptor;
+    char filename[255];
+    
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+    // Setting Permissions to Read and Write Access for the file.
+    file_descriptor = creat(filename, O_RDWR | O_CREAT);
+    if(file_descriptor != -1)
+    {
+        printf("File Created Successfully!");
+    }
+    else
+    {
+        printf("Unable to Create the File.");
+    }
+    
     return 0;
 }
