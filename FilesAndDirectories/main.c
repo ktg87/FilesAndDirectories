249b679c7dabbd1df4e8b387ab5d808e0cac8946
Keith G <kguydo@gmail.com>
2021-01-31 08:54:57 -0500

    Added some comments and error notes.

--- FilesAndDirectories/main.c
+++ FilesAndDirectories/main.c
@@ -3,20 +3,21 @@
 
 int main(int argc, const char * argv[]) {
     
-    int file_descriptor;
+    int file_descriptor; //Used to store the return value from the creat system call
     char filename[255];
     
     printf("Enter the filename: ");
     scanf("%s", filename);
     // Setting Permissions to Read and Write Access for the file.
+    // Notice the use of the bitwise OR to set two modes during the file creation
     file_descriptor = creat(filename, O_RDWR | O_CREAT);
     if(file_descriptor != -1)
     {
-        printf("File Created Successfully!");
+        printf("The File (%s) Created Successfully!\n", filename);
     }
     else
     {
-        printf("Unable to Create the File.");
+        printf("Unable to Create the File: %s.  Returned error: %d\n", filename, file_descriptor);
     }
     
     return 0;
