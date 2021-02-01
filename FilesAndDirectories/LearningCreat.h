8afdd5d2f7a1631ad3444817cb80d02f20a54921
Keith G <kguydo@gmail.com>
2021-02-01 07:34:26 -0500

    Separated	 code into separate functions so that we can get practice with each of the various system calls that are used with creating, opening, and closing files.

---
+++ FilesAndDirectories/LearningCreat.h
@@ -0,0 +1,13 @@
+//
+//  LearningCreat.h
+//  FilesAndDirectories
+//
+
+#ifndef LearningCreat_h
+#define LearningCreat_h
+
+#include <stdio.h>
+
+int useCreat(char* filename, mode_t mode);
+
+#endif /* LearningCreat_h */
--- FilesAndDirectories/main.c
+++ FilesAndDirectories/main.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include "LearningCreat.h"
 
 int main(int argc, const char * argv[]) {
     
-    int file_descriptor; //Used to store the return value from the creat system call
+    int rc = 0; //Return code
     char filename[255];
     
     printf("Enter the filename: ");
     scanf("%s", filename);
-    // Setting Permissions to Read and Write Access for the file.
-    // Notice the use of the bitwise OR to set two modes during the file creation
-    file_descriptor = creat(filename, O_RDWR | O_CREAT);
-    if(file_descriptor != -1)
-    {
-        printf("The File (%s) Created Successfully!\n", filename);
-    }
-    else
-    {
-        printf("Unable to Create the File: %s.  Returned error: %d\n", filename, file_descriptor);
-    }
     
+    mode_t mode = O_RDWR | O_CREAT;
+    
+    rc = useCreat(filename, mode);
+        
     return 0;
 }
